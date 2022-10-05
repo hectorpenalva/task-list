d945c0404359292d0ab7420a98891a7b7ee81cd1
Héctor Peñalva <hectorpenalvagarcia@icloud.com>
2022-10-05 16:53:29 +0200

    New useState to change the style of the menu If selected

--- src/components/Menu/Menu.js
+++ src/components/Menu/Menu.js
@@ -1,12 +1,25 @@
+import { useState } from "react";
 import "./Menu.css";
 
 export default function Menu() {
-
+  const [buttonSelected, setNewButtonSelected] = useState("All");
   return (
     <div className="Menu">
-      <a href="#">All</a>
-      <a href="#">Pending</a>
-      <a href="#">Completed</a>
+      <button 
+        onClick={()=>setNewButtonSelected("All")} 
+        className={buttonSelected === "All" ? "selected" : ""} 
+        href="#">All
+      </button>
+      <button 
+        onClick={()=>setNewButtonSelected("Pending")} 
+        className={buttonSelected === "Pending" ? "selected" : ""} 
+        href="#">Pending
+      </button>
+      <button 
+        onClick={()=>setNewButtonSelected("Completed")} 
+        className={buttonSelected === "Completed" ? "selected" : ""} 
+        href="#">Completed
+      </button>
     </div>
   );
 }
