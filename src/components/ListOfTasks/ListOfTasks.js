d87a4f297a9a3dc72c589044c6b9da1768e7db80
Héctor Peñalva <hectorpenalvagarcia@icloud.com>
2022-10-05 16:52:54 +0200

    Property name change

--- src/components/ListOfTasks/ListOfTasks.js
+++ src/components/ListOfTasks/ListOfTasks.js
@@ -15,7 +15,7 @@ export default function ListOfTasks(props) {
               e.id !== elemento.id
             ))
             setTasks (newArray)
-          }}> <svg width="16px" height="16px" stroke-width="1.5" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg" color="#444"><path d="M19 11v9.4a.6.6 0 01-.6.6H5.6a.6.6 0 01-.6-.6V11M10 17v-6M14 17v-6M21 7h-5M3 7h5m0 0V3.6a.6.6 0 01.6-.6h6.8a.6.6 0 01.6.6V7M8 7h8" stroke="#444" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"></path></svg></button>
+          }}> <svg width="16px" height="16px" strokeWidth="1.5" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg" color="#444"><path d="M19 11v9.4a.6.6 0 01-.6.6H5.6a.6.6 0 01-.6-.6V11M10 17v-6M14 17v-6M21 7h-5M3 7h5m0 0V3.6a.6.6 0 01.6-.6h6.8a.6.6 0 01.6.6V7M8 7h8" stroke="#444" strokeWidth="1.5" strokeLinecap="round" strokeLinejoin="round"></path></svg></button>
         </li>
 
       ))}
